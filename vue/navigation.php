c7da842fb42fdaad52c61a3aa8eaf881a4a84694
Florian TORIBIO <florian.toribio@gmail.com>
2022-09-25 15:42:17 +0200

    add profil page

--- vue/navigation.php
+++ vue/navigation.php
@@ -92,7 +92,7 @@
         </a>
       </li>
       <li class="nav__item <?php if($id_actif_navigation == 5) echo 'nav__item--selected'; ?>">
-        <a href="../controleur/FrontControleur.php?action=deconnexion" class="">
+        <a href="profil.php" class="">
             <?xml version="1.0" encoding="iso-8859-1"?>
             <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
