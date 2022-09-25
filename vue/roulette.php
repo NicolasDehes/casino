d9c0f3dd6715f1beff3d961cfc77c1ea14439e96
Florian TORIBIO <florian.toribio@gmail.com>
2022-09-25 11:07:52 +0200

    Add responsive desktop

--- vue/roulette.php
+++ vue/roulette.php
@@ -16,37 +16,41 @@
       ?>
 
     <span class="solde">Solde : 100</span>
-    <div class="roulette">
-      <div class="roulette__container" id="roulette__container">
-        <div class="roulette__item"><span class="roulette__label">x1</span></div>
-        <div class="roulette__item"><span class="roulette__label">x2</span></div>
-        <div class="roulette__item"><span class="roulette__label">x3</span></div>
-        <div class="roulette__item"><span class="roulette__label">x4</span></div>
-        <div class="roulette__item"><span class="roulette__label">x5</span></div>
-        <div class="roulette__item"><span class="roulette__label">x6</span></div>
-        <div class="roulette__item"><span class="roulette__label">x7</span></div>
-        <div class="roulette__item"><span class="roulette__label">x8</span></div>
-        <div>
-            <div class="roulette__line"></div>
-            <div class="roulette__line"></div>
-            <div class="roulette__line"></div>
-            <div class="roulette__line"></div>
+
+    <section class="body__container">
+      <div class="roulette">
+        <div class="roulette__container" id="roulette__container">
+          <div class="roulette__item"><span class="roulette__label">x1</span></div>
+          <div class="roulette__item"><span class="roulette__label">x2</span></div>
+          <div class="roulette__item"><span class="roulette__label">x3</span></div>
+          <div class="roulette__item"><span class="roulette__label">x4</span></div>
+          <div class="roulette__item"><span class="roulette__label">x5</span></div>
+          <div class="roulette__item"><span class="roulette__label">x6</span></div>
+          <div class="roulette__item"><span class="roulette__label">x7</span></div>
+          <div class="roulette__item"><span class="roulette__label">x8</span></div>
+          <div>
+              <div class="roulette__line"></div>
+              <div class="roulette__line"></div>
+              <div class="roulette__line"></div>
+              <div class="roulette__line"></div>
+          </div>
         </div>
       </div>
-    </div>
-    <button id="roulette__btn">Tourner</button>
-    <!-- TODO à implémenter -->
-    <form class="roulette-form" method="POST" action="">
-        <input type="number" min="1" max="100" class="input" placeholder="Crédits misés" required>
-        <button type="submit" class="button button--secondary button--min">Jouer</button>
-    </form>
-    <!-- TODO à implémenter QUAND ON VIENT DE JOUER
-    <section class="roulette-result">
-        <p>Vous multipliez votre mise part 5 !</p>
-        <p class="roulette-result__gain">Votre gain : <span>50€</span></p>
-        <p class="roulette-result__gain roulette-result__gain--null">Vous perdez : <span>50€</span></p>
-        <button class="button button--secondary button--min">Rejouer</button>
-    </section> -->
+      <!-- TODO à implémenter -->
+      <form class="roulette-form" method="POST" action="">
+          <input type="number" min="1" max="100" class="input" placeholder="Crédits misés" required>
+          <button type="submit" class="button button--secondary button--min">Jouer</button>
+          <button id="roulette__btn">Tourner</button>
+      </form>
+      <!-- TODO à implémenter QUAND ON VIENT DE JOUER
+      <section class="roulette-result">
+          <p>Vous multipliez votre mise part 5 !</p>
+          <p class="roulette-result__gain">Votre gain : <span>50€</span></p>
+          <p class="roulette-result__gain roulette-result__gain--null">Vous perdez : <span>50€</span></p>
+          <button class="button button--secondary button--min">Rejouer</button>
+      </section> -->
+    </section>
+    
    
     <script src="./js/roulette.js"></script>
   </body>
