5d4699a7c896e879e1e214476c34323bba81599d
Florian TORIBIO <florian.toribio@gmail.com>
2022-09-26 14:19:12 +0200

    add gitignore

--- modele/dao/Database.php
+++ modele/dao/Database.php
@@ -2,8 +2,8 @@
 
 define("DB_DRIVER", "mysql");
 define("DB_HOST", "localhost");
-define("DB_USER", "nicolas");
-define("DB_PASS", "apolline");
+define("DB_USER", "root");
+define("DB_PASS", "");
 define("DB_DATABASE", "db_test");
 define("DB_CHARSET", "utf8");
 
