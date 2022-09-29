392fcbe6691a40618a3e6117cea66916058e2dad
mdeleest <manon.deleest@etudiant.univ-lr.fr>
2022-09-29 11:58:54 +0200

    fix merge

--- controleur/FrontControleur.php
+++ controleur/FrontControleur.php
@@ -75,6 +75,11 @@ else
     // Retourner la page d'authentification
     $requested_page = 'login';
 
+    
+$userService = new UtilisateurService();
+$user = $userService->getUserById($_SESSION["id_user"]);
+$_SESSION['USER'] = $user->toArray();
+
 // En fonction de la demande de l'utilisateur, effectuer le traitement
 switch ($requested_page) {
 
@@ -160,9 +165,6 @@ switch ($requested_page) {
 
     // Afficher le jeu roulette
     case 'roulette':
-        $userService = new UtilisateurService();
-        $user = $userService->getUserById($_SESSION["id_user"]);
-        $_SESSION['USER'] = $user->toArray();
         
         // Retourner la page accueil.php : page d'accueil de l'application
         header("Location: ../vue/roulette.php");
