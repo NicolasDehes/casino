309c68962f1a956b4c2f14b785cc2f52189ac14d
HugoAlp <hugo.alpiste@gmail.com>
2022-10-21 13:18:51 +0200

    changement role admin

---
+++ vue/js/liste_utilisateurs.js
@@ -0,0 +1,19 @@
+console.log('salut')
+window.addEventListener('load', ()=>{
+    document.querySelectorAll('.tooggleAdmin').forEach((e)=>{
+    
+        e.addEventListener('change',()=>{
+            var formData = new FormData();
+            id=e.dataset.id;
+            formData.append('id', id);
+           
+            fetch('../api/admin.php?action=changeAdmin', {
+                method: 'POST',
+                body: formData,
+            }).then((id)=>id);
+            
+        })
+    });
+
+
+});
--- vue/liste_utilisateurs.php
+++ vue/liste_utilisateurs.php
@@ -8,12 +8,13 @@
         <link rel="stylesheet" type="text/css" href="css/global.css">
         <link rel="stylesheet" type="text/css" href="css/navigation.css">
         <link rel="stylesheet" type="text/css" href="css/historique.css">
+        <link rel="stylesheet" type="text/css" href="css/tooglebtn.css">
     <title>Liste des utilisateurs</title>
   </head>
   <body>
     <?php 
         $id_actif_navigation = 4; 
-        require_once("./navigation.php") 
+        require_once("./navigation.php") ;
     ?>
     <div class="banner">
         <div class="banner__content"></div>
@@ -28,16 +29,18 @@
             <td>Mail</td>
             <td>Solde</td>
             <td>Action</td>
+            <td>Admin</td>
         </thead>
         <tbody class="historique__body">
             <?php
             //$data = json_decode($_SESSION['tableau']); //to decode the string into an object
             // Tableau des utilisateurs a été positionné en variable de session
             $tableau=$_SESSION['allUsers'];
+            //var_dump($tableau);
             $color = 0;
             foreach ($tableau as $utilisateur) { ?>
                 <tr class="historique__item <?php if($color%2 != 0 ) echo 'historique__item--lost'; ?> ">
-                    <td>
+                    <td id='idRow'>
                         <?php echo $utilisateur['id']; ?>
                     </td>
                     <td>
@@ -55,10 +58,25 @@
                     <td>
                         <a href="../controleur/FrontControleur.php?action=reinitialiser_solde&id=<?= $utilisateur['id']; ?>">Reinitialiser solde</a>
                     </td>
+                    <td> 
+                        <!-- Rounded switch -->
+                        <label class="switch">
+               
+                                    <input class="tooggleAdmin" type="checkbox" data-id='<?= $utilisateur['id']?>' <?= $utilisateur['isAdmin']?"checked" : ""?> >
+                                    <span class="slider round"></span>    
+                        </label>
+                          
+                    </td>
+                  
                 </tr>
+                
                 <?php $color = $color+1 ; 
+                
             } ?>
+            
         </tbody>
     </table>
+    <script src="./js/liste_utilisateurs.js"></script>
 </body>
+
 </html>
